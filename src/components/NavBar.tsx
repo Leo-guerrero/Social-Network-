c4e9722a5a7d5047e948dba535e7f4041b4d0ce9
Leo <sja.leo.guerrero@gmail.com>
2025-06-17 03:28:18 -0500

    AHHHH

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -7,15 +7,15 @@ function NavBar(){
            
 
             <div className="">
-                <Link to="/"><button><span className="px-4 text-lg RoobootMedium">The Social Network</span></button></Link>
+                <Link to="/"><button><span style={{ fontFamily: 'Roboot-Medium' }} className="px-4 text-lg">The Social Network</span></button></Link>
                 <button className="NavButton">About</button>
                 <button className="NavButton">Contact</button>
                 <Link to="/backendtest"><button className="NavButton">Backend</button></Link>
             </div>
 
             <div className="absolute right-4 flex gap-x-2">
-                <button className="text-sm border border-gray-800 p-2 px-4 rounded-lg hover:bg-purple-500 hover:text-black RoobootLight">Sign up</button>
-                <button className="text-sm border border-gray-800 p-2 px-4 rounded-lg hover:bg-purple-500 hover:text-black RoobootLight">Login</button>
+                <button style={{ fontFamily: 'Roboot-light' }} className="text-sm border border-gray-800 p-2 px-4 rounded-lg hover:bg-purple-500 hover:text-black">Sign up</button>
+                <button style={{ fontFamily: 'Roboot-light' }} className="text-sm border border-gray-800 p-2 px-4 rounded-lg hover:bg-purple-500 hover:text-black">Login</button>
             </div>
 
         </div>
--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -2,14 +2,14 @@ function LandingPage(){
     return (
         <div className="p-20 space-y-8 w-1/2">
             
-            <div className="text-5xl md:text-6xl lg:text-7xl RoobootMedium space-y-2">
+            <div style={{ fontFamily: 'Roboot-Medium' }} className="text-5xl md:text-6xl lg:text-7xl space-y-2">
                 <div>Connect.</div>
                 <div className="text-purple-500">&lt;Code.&nbsp;/&gt;</div>
                 <div>Collaborate.</div>
             </div>
 
-            <div className="text-xl RoobootLight">blhlasdlfj sljfls kdjflsk jsdlk fsd f sjdfl sjlfksdj lfskdj ls</div>
-            <div><button className="text-md RoobootLight border border-white p-4 hover:bg-purple-500 hover:border-purple-500">Get Started</button></div>
+            <div style={{ fontFamily: 'Roboot-light' }} className="text-xl">blhlasdlfj sljfls kdjflsk jsdlk fsd f sjdfl sjlfksdj lfskdj ls</div>
+            <div><button style={{ fontFamily: 'Roboot-light' }} className="text-md border border-white p-4 hover:bg-purple-500 hover:border-purple-500">Get Started</button></div>
         </div>
     )
 
