ad64779ce2d5463e330703a02c328362a9455207
Evan Ebert <evan-ebert17@gmail.com>
2025-09-15 22:38:52 -0500

    changed react pages MainFeed.tsx and postPage.tsx to reflect reply functionality

--- src/pages/MainFeed.tsx
+++ src/pages/MainFeed.tsx
@@ -35,6 +35,7 @@ function MainFeed(){
 
     type Post = {
         id: number;
+        parentId: null;
         userid: number;
         text: string;
         createdAt: string;
@@ -99,6 +100,8 @@ function MainFeed(){
 
     }, [posts]);
     console.log("Post tuah:", posts);
+
+    // const topLevelPosts = posts.filter(post => !post.parentId);
    
     
     return (
